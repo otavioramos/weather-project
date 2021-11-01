eca7e7249fd22b1f8d04ac94d6701b26772a69c7
Otavio Ramos <otavio.omak@gmail.com>
2021-11-01 20:59:10 -0300

    Pegar o resultado da requisição e utilizá-lo

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const app = express();
 app.get("/", (req, res) => {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=8d729619f8e1c9e16ef07a4c9fe7b5e4&units=metric";
     https.get(url,(response) => {
-        console.log(response);
+       response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            console.log(description);
+       });
     });
+
     res.send("Server is up and running");
 });
 
