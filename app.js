59d874cd934b69bb9a064d18794512717050dcf5
Otavio Ramos <otavio.omak@gmail.com>
2021-11-01 21:23:04 -0300

    Alterando as mensagens de acordo com a resposta da api

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ const https = require("https");
 
 const app = express();
 
+
 app.get("/", (req, res) => {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=8d729619f8e1c9e16ef07a4c9fe7b5e4&units=metric";
+    const city = "Sao Paulo"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8d729619f8e1c9e16ef07a4c9fe7b5e4&units=metric`;
     https.get(url,(response) => {
        response.on("data", (data) => {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
-            console.log(description);
+            const icon = weatherData.weather[0].icon;
+            const imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+            res.write(`<p>The weather is currently ${description}.</p>`);
+            res.write(`<h1>The temperature in ${city} is ${temp} degrees Celsius.</h1>`);
+            res.write(`<img src="${imageUrl}" alt="weather-image">`);
+            res.send();
        });
     });
-
-    res.send("Server is up and running");
 });
 
 app.listen(3000, () => {
